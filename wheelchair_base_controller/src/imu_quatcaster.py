e4889a30825ccef16cbf10c168f3d590b325b62a
Chad Rockey <chadrockey@gmail.com>
2011-05-12 20:00:39 -0400

    Updated the IMU reader to be more stable and no longer crash on startup.  Also edited the Arduino code to start sending data faster.

--- wheelchair_base_controller/src/imu_quatcaster.py
+++ wheelchair_base_controller/src/imu_quatcaster.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import roslib
-roslib.load_manifest("sparkfun_9dof_razor")
+roslib.load_manifest("wheelchair_base_controller")
 import rospy
 
 from sensor_msgs.msg import Imu
@@ -26,31 +26,34 @@ class SF9DOF_Broadcaster:
 
     def loop(self):
         while not rospy.is_shutdown():
-            line = port.readline()
+            line = self.port.readline()
             chunks = line.split(":")
             if chunks[0] == "!QUAT":
                 readings = chunks[1].split(",")
-                imu_msg = Imu()
-		imu_msg.header.stamp = rospy.Time.now()
-		imu_msg.header.frame_id = 'imu'
-		imu_msg.orientation.x = float(readings[0])
-		imu_msg.orientation.y = float(readings[1])
-		imu_msg.orientation.z = float(readings[2])
-		imu_msg.orientation.w = float(readings[3])
-		imu_msg.orientation_covariance = list(zeros((3,3)).flatten())
-		imu_msg.angular_velocity.x = float(readings[4])
-		imu_msg.angular_velocity.y = float(readings[5])
-		imu_msg.angular_velocity.z = float(readings[6])
-		imu_msg.angular_velocity_covariance = list(0.1*diagflat(ones((3,1))).flatten())
-		imu_msg.linear_acceleration.x = float(readings[7])
-		imu_msg.linear_acceleration.y = float(readings[8])
-		imu_msg.linear_acceleration.z = float(readings[9])
-		imu_msg.linear_acceleration_covariance = list(0.1*diagflat(ones((3,1))).flatten())
-		self.pub.publish(imu_msg)
-		quaternion = (imu_msg.orientation.x, imu_msg.orientation.y, imu_msg.orientation.z, imu_msg.orientation.w)
-		tf_br.sendTransform(translation = (0,0, 0), rotation = quaternion,time = rospy.Time.now(),child = 'imu',parent = 'world')
-            else:
-                rospy.logerr("Did not get a valid IMU packet, got %s", line)
+                if len(readings) == 10:
+		    imu_msg = Imu()
+		    imu_msg.header.stamp = rospy.Time.now()
+		    imu_msg.header.frame_id = 'imu'
+		    imu_msg.orientation.x = float(readings[0])
+		    imu_msg.orientation.y = float(readings[1])
+		    imu_msg.orientation.z = float(readings[2])
+		    imu_msg.orientation.w = float(readings[3])
+		    imu_msg.orientation_covariance = list(zeros((3,3)).flatten())
+		    imu_msg.angular_velocity.x = float(readings[4])
+		    imu_msg.angular_velocity.y = float(readings[5])
+		    imu_msg.angular_velocity.z = float(readings[6])
+		    imu_msg.angular_velocity_covariance = list(0.1*diagflat(ones((3,1))).flatten())
+		    imu_msg.linear_acceleration.x = float(readings[7])
+		    imu_msg.linear_acceleration.y = float(readings[8])
+		    imu_msg.linear_acceleration.z = float(readings[9])
+		    imu_msg.linear_acceleration_covariance = list(0.1*diagflat(ones((3,1))).flatten())
+		    self.pub.publish(imu_msg)
+		    quaternion = (imu_msg.orientation.x, imu_msg.orientation.y, imu_msg.orientation.z, imu_msg.orientation.w)
+		    tf_br.sendTransform(translation = (0,0, 0), rotation = quaternion,time = rospy.Time.now(),child = 'imu',parent = 'world')
+		else:
+		    rospy.logerr("Did not get a valid IMU packet, got %s", line)
+	    else:
+		rospy.loginfo("Did not receive IMU data, instead got %s", line)
 
 if __name__ == "__main__":
     rospy.init_node('imu_quatcaster')
