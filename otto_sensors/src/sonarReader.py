859389056f6698c94ecdf017e97ef436dac5dccb
otto <otto@ottoboti5.(none)>
2011-05-20 03:41:43 -0400

    Added the working version of sonarReader.py from Otto.

---
+++ otto_sensors/src/sonarReader.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import roslib; roslib.load_manifest('sonar_joy')
+import rospy
+import serial
+from sensor_msgs.msg import Range
+
+def sendMessage(sonarId, distance):
+	sonarDictionary = {'1':"frontLeftSonar", '2':"diagLeftSonar", '3':"sideLeftSonar", '4':"frontRightSonar", '5':"diagRightSonar", '6':"sideRightSonar"}
+	sonarName = sonarDictionary[sonarId]
+	sonar = Range()
+	sonar.header.stamp = rospy.Time.now()
+	sonar.header.frame_id = sonarName
+	sonar.range = float(distance)
+	sonar.field_of_view = .3489
+	sonar.max_range = 6.45
+	sonar.min_range = .1524
+	return sonar
+
+def talker():
+	pub = rospy.Publisher('sonars', Range)
+	rospy.init_node('sonarReader')
+	port_name = rospy.get_param("~port_name", "/dev/ttyUSB0")
+	port = serial.Serial(port_name, 115200)
+	for i in range(0,10):
+		line = port.readline()
+
+	while not rospy.is_shutdown():
+		line = port.readline()
+		#print repr(line)
+		chunks = line.split(",")
+		if len(chunks) == 2:
+			#print "Length was 2"
+			message = sendMessage(chunks[0],chunks[1])
+			pub.publish(message)
+	port.close()
+
+if __name__ == '__main__':
+	try:
+		talker()
+	except rospy.ROSInterruptException: pass
