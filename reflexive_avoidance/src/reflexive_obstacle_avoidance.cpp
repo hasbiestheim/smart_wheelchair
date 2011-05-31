6d20599e2d430998bf70197afa78e304a84e1b07
Chad Rockey <chadrockey@gmail.com>
2011-05-31 03:47:29 -0400

    This node should be all ready to go.  Just needs parameter adjustment.

--- reflexive_avoidance/src/reflexive_obstacle_avoidance.cpp
+++ reflexive_avoidance/src/reflexive_obstacle_avoidance.cpp
@@ -1,4 +1,6 @@
 #include <ros/ros.h>
+#include <dynamic_reconfigure/server.h>
+#include <reflexive_avoidance/ReflexConfig.h>
 #include <std_msgs/String.h>
 #include <joy/Joy.h>
 #include <std_msgs/Bool.h>
@@ -8,72 +10,77 @@
 #include <pcl/point_types.h>
 
 // PARAMETERS
-int frontLaserCenter = 180;
-int frontLaserWindow = 20;
-int frontLaserDist = 0.5;
-int rearLaserCenter = 0;
-int rearLaserWindow = 10;
-int rearLaserDist = 1.3;
+double frontLaserDist_;
+double frontLaserAngle_;
+double sideLaserDist_;
+double sideLaserAngle_;
 
-int numberOfLaserBeams = 360;
+float rangeFrontThreshold_;
+float rangeSideThreshold_;
 
-float rangeFrontThreshold = 0.51;
-float rangeSideThreshold = 0.44;
-
-int kinectPoints = 1000;
+int kinectPoints_;
 
 // CALLBACKS
 
 bool laserFrontAllow = true;
-bool laserRearAllow = true;
+bool laserLeftAllow = true;
+bool laserRightAllow = true;
+
+bool uselaser_;
 
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
 	// Start by assuming a clear front, yay optimism!
 	laserFrontAllow = true;
+	laserLeftAllow = true;
+	laserRightAllow = true;
+	
+	float centerPoint = (msg->angle_max - msg->angle_min)/2.0;
+	
+	float frontLimitLow = centerPoint - frontLaserAngle_/2.0;
+	float frontLimitHigh = centerPoint + frontLaserAngle_/2.0;
+	
+	float rightLimitLow = msg->angle_min;
+	float rightLimitHigh = msg->angle_min + sideLaserAngle_;
+	
+	float leftLimitLow = msg->angle_max - sideLaserAngle_;
+	float leftLimitHigh = msg->angle_max;
 
-	// For certain beams up front (around ping 180 since reversed), find and report the min
-	float minRange = msg->range_min;
-	for(int i = -frontLaserWindow/2; i <= frontLaserWindow/2; i++){
-
-		int checkNum = frontLaserCenter + i;
-		if(checkNum < 0){
-			checkNum += numberOfLaserBeams;
-		} else if(checkNum >= numberOfLaserBeams){
-			checkNum -= numberOfLaserBeams;
-		}
-
-		float beamRange = msg->ranges[frontLaserCenter + i];
-		if((beamRange < frontLaserDist) && (beamRange > minRange)){
-			laserFrontAllow = false;  // Oops, found a dangerous return
-			ROS_DEBUG("Danger according to Front Laser");
-			break;
-		}
-	}
-
-	laserRearAllow = true;	
-
-	for(int i = -rearLaserWindow/2; i <= rearLaserWindow/2; i++){
-
-		int checkNum = rearLaserCenter + i;
-		if(checkNum < 0){
-			checkNum += numberOfLaserBeams;
-		} else if(checkNum >= numberOfLaserBeams){
-			checkNum -= numberOfLaserBeams;
-		}
-
-		float beamRange = msg->ranges[checkNum];
-		if((beamRange < rearLaserDist) && (beamRange > minRange)){
-			laserRearAllow = false;  // Oops, found a dangerous return
-			ROS_DEBUG("Danger according to Rear Laser");
-			break;
-		}
+	float angle = msg->angle_min;
+	for(uint i = 0; i <= msg->ranges.size(); i++){
+	  // Check if valid beam
+	  float range = msg->ranges[i];
+	  if(range >= msg->range_min && range <= msg->range_max){
+	    // Check if within center range
+	    if(angle >= frontLimitLow && angle <= frontLimitHigh && laserFrontAllow){
+	      if(range < frontLaserDist_){
+		laserFrontAllow = false;
+		ROS_DEBUG("Danger according to Front Laser.");
+	      }
+	    }
+	    // Check if within right range
+	    else if(angle >= rightLimitLow && angle <= rightLimitHigh && laserRightAllow){
+	      if(range < sideLaserDist_){
+		laserRightAllow = false;
+		ROS_DEBUG("Danger according to Right Laser.");
+	      }
+	    }
+	    // Check if within left range
+	    else if(angle >= leftLimitLow && angle <= leftLimitHigh && laserLeftAllow){
+	      if(range < sideLaserDist_){
+		laserLeftAllow = false;
+		ROS_DEBUG("Danger according to Left Laser.");
+	      }
+	    }
+	  }
 	}	
 }
 
 bool rangerFrontAllow = true;
 bool rangerLeftAllow = true;
 bool rangerRightAllow = true;
+
+bool useranger_;
 
 void rangeCallback(const sensor_msgs::Range::ConstPtr& msg)
 {
@@ -84,7 +91,7 @@ void rangeCallback(const sensor_msgs::Range::ConstPtr& msg)
 	if(range >= msg->min_range){
 		// Compare each sonar
 		if(frameID == "frontLeftSonar" || frameID == "frontRightSonar"){ 
-			if(range < rangeFrontThreshold){
+			if(range < rangeFrontThreshold_){
 				rangerFrontAllow = false;
 				ROS_DEBUG("Danger according to Front Rangers");
 			} else {
@@ -93,14 +100,14 @@ void rangeCallback(const sensor_msgs::Range::ConstPtr& msg)
 		} else if(frameID == "diagLeftSonar" || frameID == "diagRightSonar"){
 
 		} else if(frameID == "sideLeftSonar"){
-			if(range < rangeSideThreshold){
+			if(range < rangeSideThreshold_){
 				rangerLeftAllow = false;
 				ROS_DEBUG("Danger according to Side Left Ranger");
 			} else {
 				rangerLeftAllow = true;
 			}
 		} else if(frameID == "sideRightSonar"){
-			if(range < rangeSideThreshold){
+			if(range < rangeSideThreshold_){
 				rangerRightAllow = false;
 				ROS_DEBUG("Danger according to Side Right Laser");
 			} else {
@@ -109,6 +116,8 @@ void rangeCallback(const sensor_msgs::Range::ConstPtr& msg)
 		}
 	}
 }
+
+bool usekinect_;
 
 bool kinectAllow = true;
 
@@ -120,9 +129,9 @@ void pointcloudCallback(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& cloud)
 
 	//ROS_INFO_STREAM("Number of Kinect Points: " << currentPoints);
 
-	if((int)currentPoints > kinectPoints){
+	if((int)currentPoints > kinectPoints_){
 		kinectAllow = false;
-		ROS_DEBUG("Danger according to Front Kinect");
+		ROS_DEBUG("Danger according to Kinect");
 	} else {
 		kinectAllow = true;
 	}
@@ -145,37 +154,51 @@ void joyCallback(const joy::Joy::ConstPtr& msg)
 	std_msgs::Bool collision_msg;
 	collision_msg.data = true;
 
-	if((!laserFrontAllow || !rangerFrontAllow || !kinectAllow) && (newJoy.axes[1] > 0.0)){
+	if(((!laserFrontAllow && uselaser_) || (!rangerFrontAllow && useranger_) || (!kinectAllow && usekinect_)) && (newJoy.axes[1] > 0.0)){
 		newJoy.axes[1] = 0.0;
 		collision_warn_pub.publish(collision_msg);
 	}
-	if(!laserRearAllow && (newJoy.axes[1] < 0.0)){
-		newJoy.axes[1] = 0.0;
-		collision_warn_pub.publish(collision_msg);
-	}
-	if(!rangerLeftAllow && (newJoy.axes[0] > 0.0)){
+	if(((!laserLeftAllow && uselaser_) || (!rangerLeftAllow && useranger_)) && (newJoy.axes[0] > 0.0)){
 		newJoy.axes[0] = 0.0;
 		collision_warn_pub.publish(collision_msg);
 	}
-	if(!rangerRightAllow && (newJoy.axes[0] < 0.0)){
+	if(((!laserRightAllow && uselaser_) || (!rangerRightAllow && useranger_)) && (newJoy.axes[0] < 0.0)){
 		newJoy.axes[0] = 0.0;
 		collision_warn_pub.publish(collision_msg);
 	}
-	
 	joyout_pub.publish(newJoy);
+}
+
+void callback(reflexive_avoidance::ReflexConfig &config, uint32_t level)
+{
+  usekinect_ = config.use_kinect;
+  kinectPoints_ = config.num_points;
+  useranger_ = config.use_sonars;
+  rangeFrontThreshold_ = config.front_sonar_thresh;
+  rangeSideThreshold_ = config.side_sonar_thresh;
+  uselaser_ = config.use_laser;
+  frontLaserDist_ = config.front_laser_thresh;
+  frontLaserAngle_ = config.front_laser_angle;
+  sideLaserDist_ = config.side_laser_thresh;
+  sideLaserAngle_ = config.side_laser_angle;
 }
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "new_joystick_limiter");
   ros::NodeHandle n;
-  joyout_pub = n.advertise<joy::Joy>("joy_output", 1);
   
   collision_warn_pub = n.advertise<std_msgs::Bool>("collision_warning", 1);
+  joyout_pub = n.advertise<joy::Joy>("joy_output", 1);
+    
+  dynamic_reconfigure::Server<reflexive_avoidance::ReflexConfig> srv;
+  dynamic_reconfigure::Server<reflexive_avoidance::ReflexConfig>::CallbackType f;
+  f = boost::bind(&callback, _1, _2);
+  srv.setCallback(f);
+  
+  ros::Subscriber laser_sub = n.subscribe("scan", 100, laserCallback);
 
-  //ros::Subscriber laser_sub = n.subscribe("scan", 100, laserCallback);
-
-  ros::Subscriber ranger_sub = n.subscribe("sonars", 100, rangeCallback);
+  ros::Subscriber ranger_sub = n.subscribe("range", 100, rangeCallback);
 
   ros::Subscriber kinect_sub = n.subscribe("input_cloud", 1, pointcloudCallback);
 
